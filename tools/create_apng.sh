13703f19b45a54027f166a19e741df37c71ae3c8
eddie-splatoon <eddie.splatoon@gmail.com>
2025-12-17 00:27:06 +0900

    fix: Demo APNG

--- tools/create_apng.sh
+++ tools/create_apng.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 #ffmpeg -ss 5 -t 10 -i input.mov -vf "fps=12,scale=800:-1" -pix_fmt rgba -f apng -plays 0 demo.png
-ffmpeg -ss 0 -t 10 -i input.mov -vf "setpts=0.5*PTS,fps=12,scale=800:-1" -pix_fmt rgba -f apng -plays 0 demo.png
+#ffmpeg -ss 0 -t 10 -i input.mov -vf "setpts=0.5*PTS,fps=12,scale=800:-1" -pix_fmt rgba -f apng -plays 0 demo.png
+
+ffmpeg -i input.mov \
+-vf "setpts=0.33*PTS,fps=12,scale=800:-1" \
+-pix_fmt rgba \
+-f apng \
+-plays 0 \
+demo.png
